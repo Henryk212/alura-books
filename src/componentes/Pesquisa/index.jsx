14504c381755a1299b77d4b9f63ee7ede64930d0
Henryk212 <111775517+Henryk212@users.noreply.github.com>
2023-07-12 14:28:45 -0300

    Botão pesquisa

--- src/componentes/Pesquisa/index.jsx
+++ src/componentes/Pesquisa/index.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import Input from '../Input'
+import styled from 'styled-components'
+
+const PesquisaContainer = styled.section`
+        background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+        color: #FFF;
+        text-align: center;
+        padding: 85px 0;
+        height: 270px;
+        width: 100%;
+`
+const Titulo = styled.h2`
+        color: #FFF;
+        font-size: 36px;
+        text-align: center;
+        width: 100%;
+`
+const Subtitulo = styled.h3`
+        font-size: 16px;
+        font-weight: 500;
+        margin-bottom: 40px;
+`
 
 export default function Pesquisa() {
   return (
-    <section>
-    <h2>Já sabe por onde começar ?</h2>
-    <h3>Encontre seu livro em nossa estate!</h3>
-    <input
+    <PesquisaContainer> 
+    <Titulo>Já sabe por onde começar ?</Titulo>
+    <Subtitulo>Encontre seu livro em nossa estate!</Subtitulo>
+    <Input
         placeholder='Escreva sua proxima leitura'
     />
-    </section>
+    </PesquisaContainer>
   )
 }
